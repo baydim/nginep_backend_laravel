d1b60df8e870b99090f7c31601cb1656e274f66f
baydim <baydim56@gmail.com>
2021-06-26 21:03:29 +0700

    init

---
+++ app/Http/Controllers/Auth/Auth_controller.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class Auth_controller extends Controller
+{
+
+    /////login
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+    /////login
+
+    /////registrasi
+    public function regis(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\Auth_controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HotelController;
-// use App\Http\Controllers\HotelFacilController;
-use App\Http\Controllers\RoomController;
-// use App\Http\Controllers\mulimage;
-
-
-
 use App\Http\Controllers\Hotel_Controller;
 use App\Http\Controllers\Room_Controller;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -28,38 +23,26 @@ use App\Http\Controllers\Room_Controller;
 // });
 
 
-////hotel
-// Route::get('/hotel', [HotelController::class, 'index']);
-// Route::get('/hotel/idhotel={id}', [HotelController::class, 'hoteldetail']);
-// Route::get('hotel/cari/alamat={alamat}', [HotelController::class, 'carihotel']);
-// Route::post('hotel/add/', [HotelController::class, 'addhotel']);
+
+///auth
+Route::post('/login', [Auth_controller::class, 'login']);
+Route::post('/regis', [Auth_controller::class, 'regis']);
 
 
-// /////roomcontroller
-// Route::get('/hotel/rooms/idrooms={id}', [RoomController::class, 'detail']);
-// Route::post('/hotel/rooms/add', [RoomController::class, 'addroom']);
-// Route::get('/hotel/rooms/delete/idrooms={id}', [RoomController::class, 'deleteroom']);
+///route groub sactum
 
+Route::group(['middleware' => 'auth:sanctum'], function () {
 
-// /////hotelfacilcontroller
-// Route::get('/hotel/facil/hotel_id={hotel_id}', [HotelFacilController::class, 'facil']);
-// Route::get('/facil/hapus/facil_id={facil_id}', [HotelFacilController::class, 'deletfacil']);
-
-
-// //tesmultiple
-// Route::post('/mul', [mulimage::class, 'mul']);
+    ////new
+    Route::get('/hotel', [Hotel_Controller::class, 'allHotel']);
+    Route::get('/hotel/id={id}', [Hotel_Controller::class, 'detailHotel']);
+    Route::get('/hotel/delete/id={id}', [Hotel_Controller::class, 'deletHotel']);
+    Route::post('hotel/add/', [Hotel_Controller::class, 'addHotel']);
 
 
 
-////new
-Route::get('/hotel', [Hotel_Controller::class, 'allHotel']);
-Route::get('/hotel/id={id}', [Hotel_Controller::class, 'detailHotel']);
-Route::get('/hotel/delete/id={id}', [Hotel_Controller::class, 'deletHotel']);
-Route::post('hotel/add/', [Hotel_Controller::class, 'addHotel']);
-
-
-
-///room
-Route::get('/room', [Room_Controller::class, 'allRoom']);
-Route::get('/room/id={id}', [Room_Controller::class, 'detailRoom']);
-Route::post('/room/add/', [Room_Controller::class, 'addRoom']);
+    ///room
+    Route::get('/room', [Room_Controller::class, 'allRoom']);
+    Route::get('/room/id={id}', [Room_Controller::class, 'detailRoom']);
+    Route::post('/room/add/', [Room_Controller::class, 'addRoom']);
+});
